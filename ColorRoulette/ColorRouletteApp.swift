6b0732aaa6987d71d294a7d44f72af3ca3158948
itouuuuuuuuu <mfito.13@gmail.com>
2024-05-04 03:00:15 +0900

    Initial Commit

---
+++ ColorRoulette/ColorRouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorRouletteApp.swift
+//  ColorRoulette
+//
+//  Created by Masafumi Ito on 2024/05/04.
+//
+
+import SwiftUI
+
+@main
+struct ColorRouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorRoulette/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColorRoulette
+//
+//  Created by Masafumi Ito on 2024/05/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
